4124aa19a07dee99823b84b63d66700d9d1e4975
Akın <aknyldrm361@gmail.com>
2023-06-15 22:47:38 +0300

    dimension dictionary

---
+++ Assets/Scripts/DimensionDictionary.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using _Scripts.Tiles;
+using UnityEngine;
+
+public static class DimensionDictionary
+{
+    private static readonly Dictionary<PlaceableDimension, Vector2[]> dimensionDic = new Dictionary<PlaceableDimension, Vector2[]>()
+    {
+        { PlaceableDimension.OneByOne, new []{Vector2.zero }},
+        { PlaceableDimension.TwoByThree, new []{ Vector2.right, Vector2.up, Vector2.down, Vector2.right + Vector2.up, Vector2.right + Vector2.down}},
+        { PlaceableDimension.FourByFour, new []{ Vector2.up, Vector2.right, Vector2.right + Vector2.up }}
+    };
+
+    public static Vector2[] GetDirectionByDimension(this NodeBase nodeBase, PlaceableDimension dimension)
+    {
+        return dimensionDic[dimension];
+    }
+}
