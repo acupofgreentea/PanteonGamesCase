0dbd55a5337427f3babd9cf8e91a28c8bc769c9f
Akın <aknyldrm361@gmail.com>
2023-06-17 21:54:36 +0300

    build manager

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using _Scripts.Tiles;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    [field: SerializeField] public BuildingBase CurrentBuilding { get; private set; }
+    
+    public static BuildManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void Build(bool isAbleToBuild, NodeBase nodeBase)
+    {
+        if (!isAbleToBuild)
+        {
+            Debug.LogError("You cannot build here!");
+            return;
+        }
+
+        var build = Instantiate(CurrentBuilding);
+        build.transform.position = nodeBase.GetPositionByDimension(build.Dimension);
+    }
+}
--- Assets/Scripts/BuildingBase.cs
+++ Assets/Scripts/BuildingBase.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
-using _Scripts.Tiles;
 using UnityEngine;
 
 public abstract class BuildingBase : MonoBehaviour
@@ -14,25 +11,5 @@ public abstract class BuildingBase : MonoBehaviour
     protected virtual void Start()
     {
         buildingVisual.sprite = PlaceableSo.PlaceableSprite;
-    }
-
-    public bool IsAbleToBuild(NodeBase currentNode)
-    {
-        Vector2[] directions = currentNode.GetDirectionByDimension(Dimension);
-
-        List<NodeBase> list = GridSelectionManager.Instance.GetNodesAtDirections(currentNode, directions);
-
-        list.Add(currentNode);
-
-        if (list.Any(x => x.IsAvailable == false))
-            return false;
-
-        if (list.Count - 1 == directions.Length)
-            return false;
-
-        foreach (NodeBase nodeBase in list)
-            nodeBase.IsAvailable = false;
-
-        return true;
     }
 }
